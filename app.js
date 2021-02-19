38fa6e9496695ecf65cecb7b012aa3a43447869c
Andrew-Irving <amirving94@gmail.com>
2021-02-18 20:46:12 -0500

    added eur convert function

--- app.js
+++ app.js
@@ -17,12 +17,20 @@ clearBtn.addEventListener('click', () => {
 })
 
                 // Functions //
-
+// event function
 function converter() {
-     usd()
+    // USD convert function
+    usd()
+    // EUR convert function
+    eur()
+
 }
 
-// Currency Functions
+function test() {
+    console.log('test')
+}
+
+// currency functions
 function usd() {
     let total
 
@@ -54,6 +62,46 @@ function usd() {
         // USD to CHF
         case output.value == 7:
             total = input.value * 0.09
+        default:
+            undefined
+            break   
+         }
+    test()
+    console.log(total)
+    
+}
+
+function eur() {
+    let total
+
+    switch (current.value == 2) {
+        // EUR to USD
+        case output.value == 1:
+            total = input.value * 1.21
+            break
+        // EUR to EUR
+        case output.value == 2:
+            total = input.value * 1
+            break
+        // EUR to JPY
+        case output.value == 3:
+            total = input.value * 127.79
+            break
+        // EUR to GDP
+        case output.value == 4:
+            total = input.value * 0.87
+            break
+        // EUR to AUD
+        case output.value == 5:
+            total = input.value * 1.56
+            break
+        // EUR to CAD
+        case output.value == 6:
+            total = input.value * 1.54
+            break
+        // EUR to CHF
+        case output.value == 7:
+            total = input.value * 1.08
         default:
             break
     }
